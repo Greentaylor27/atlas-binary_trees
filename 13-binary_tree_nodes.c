709e745254ac927b0864cb760fdfe4d38cdc8206
Greentaylor27 <144153880+Greentaylor27@users.noreply.github.com>
2024-04-16 02:36:05 +0000

    counting nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - count nodes with atleast 1 child
+ * @tree: root of tree
+ *
+ * Return: number of nodes with atleast 1 child
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		count = 1;
+	}
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+
+	return (count);
+}
