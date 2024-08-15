e92460dee136c7dc22ff847280dfb02112e93215
Greentaylor27 <greentaylor18@gmail.com>
2024-08-15 19:33:49 +0000

    is leaf revision?

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,10 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node && node->left == NULL & node->right == NULL)
+	if (node != NULL)
+		return (1);
+
+	if (node && node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
 }
