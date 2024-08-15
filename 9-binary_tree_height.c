4e3071d378237cbae727c1658053bac2327f2935
Greentaylor27 <greentaylor18@gmail.com>
2024-08-15 21:24:54 +0000

    first try?

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - is used to determine the longest branch
+ *
+ * @tree: pointer to root node
+ * Return: on success the height of the tree. on failure 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l, r = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL)
+		l = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		r = binary_tree_height(tree->right) + 1;
+
+	return (l > r ? l : r);
+}
