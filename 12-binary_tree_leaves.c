73beeb018bd2faedec4173083af2fa351dcc4348
Greentaylor27 <greentaylor18@gmail.com>
2024-08-15 22:09:46 +0000

    always forgetting to remove unused variable after making changes to logic

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,7 +10,6 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	int check = binary_tree_is_leaf(tree);
 	size_t leaves = 0;
 
 	if (!tree)
@@ -21,6 +20,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	leaves += binary_tree_leaves(tree->left);
 	leaves += binary_tree_leaves(tree->right);
-	
+
 	return (leaves);
 }
