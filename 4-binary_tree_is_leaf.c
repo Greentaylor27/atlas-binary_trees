9f22dbb84243f1b2c1276cd9b92fc895a283ec28
Greentaylor27 <greentaylor18@gmail.com>
2024-08-15 19:30:07 +0000

    is leaf?

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks to see if the node is a leaf (End of the tree)
+ *
+ * @node: the node you are searching for
+ *
+ * return: 0 on success. 1 on failure
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node && node->left != NULL & node->right != NULL)
+		return (0);
+	return (1);
+}
