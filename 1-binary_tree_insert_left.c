b005ccd58edab9f45407d256eb2d71b08612b66a
Greentaylor27 <greentaylor18@gmail.com>
2024-08-15 17:01:51 +0000

    Attempt at insertion to left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,17 +12,19 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *NewNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *NewNode = binary_tree_node(parent, value);
 
-	if (NewNode == NULL)
-		return (NULL);
 	if (parent == NULL)
 		return (NULL);
+	if (NewNode == NULL)
+		return (NULL);
 
-	NewNode->n = value;
-	NewNode->parent = parent;
-	NewNode->left = parent;
-	NewNode->right = NULL;
+	if (parent->left != NULL)
+	{
+		NewNode->left = parent->left;
+		parent->left->parent = NewNode;
+	}
+	parent->left = NewNode;
 
 	return (NewNode);
 }
