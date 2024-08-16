2700349dac725a30d520f7fa4cd0c6481af7d411
Greentaylor27 <greentaylor18@gmail.com>
2024-08-16 14:08:46 +0000

    first attempt at sibling and adding sibling and uncle to header file

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - finds the sibling node in a binary tree
+ *
+ * @node: pointer to the node you trying to find the sibling of
+ * Return: pointer to sibling node. NULL on failure or empty
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *sibling;
+
+	if (node == NULL || (node->parent) == NULL)
+		return (NULL);
+	
+	sibling = node->parent->left;
+	if (!sibling)
+		return (NULL);
+	sibling = node->parent->right;
+	if (!sibling)
+		return (NULL);
+	return (sibling);
+}
