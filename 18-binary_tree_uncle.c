15025e0363fe9c7fd7a7a756b481a2a0c4f462fb
Greentaylor27 <greentaylor18@gmail.com>
2024-08-16 14:53:11 +0000

    forgot to add is sibling func to code

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,4 +1,26 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling node in a binary tree (helper func)
+ *
+ * @node: pointer to the node you trying to find the sibling of
+ * Return: pointer to sibling node. NULL on failure or empty
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *sibling;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left != NULL && node->parent->left == node)
+		sibling = node->parent->right;
+	if (node->parent->right != NULL && node->parent->right == node)
+		sibling = node->parent->left;
+	return (sibling);
+}
+
 /**
  * binary_tree_uncle - finds the uncle node to a node in a binary tree
  *
